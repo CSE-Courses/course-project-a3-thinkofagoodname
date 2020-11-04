3f1cf09d01a636f91c070aa52d8151b8d87f00a8
unknown <jmccart@buffalo.edu>
2020-11-04 13:14:56 -0500

    #61 Add visualization for notes and key bindings in editor

--- groupsite/general/static/js/tone.js
+++ groupsite/general/static/js/tone.js
@@ -1,5 +1,5 @@
 console.clear();
-let keyboardPosition = 4,
+let 
     synthType = "new Tone.Synth().toMaster()",
     soundSelected = "Synth",
     oscillatorType = "triangle",
@@ -36,130 +36,131 @@ const determineNote = (note) => {
   selectSynthSound();
 
   switch(note) {
-    /*case 122:
-      keyboardPosition--;
-      break;
-    case 120:
-      keyboardPosition++;
-      break;*/
+    
+
+
+
+
+
     case 97:
-      synth.triggerAttackRelease("C" + keyboardPosition, "8n");
-      whiteKeyPress(".C");
+      synth.triggerAttackRelease("C" + 4 , "8n");
+      wKPress(".C");
       break;
     case 115:
-      synth.triggerAttackRelease("D" + keyboardPosition, "8n");
-      whiteKeyPress(".D");
+      synth.triggerAttackRelease("D" + 4, "8n");
+      wKPress(".D");
       break;
     case 100:
-      synth.triggerAttackRelease("E" + keyboardPosition, "8n");
-      whiteKeyPress(".E");
+      synth.triggerAttackRelease("E" + 4, "8n");
+      wKPress(".E");
       break;
     case 102:
-      synth.triggerAttackRelease("F" + keyboardPosition, "8n");
-      
+      synth.triggerAttackRelease("F" + 4, "8n");
+      wKPress(".F");
       break;
     case 103:
-      synth.triggerAttackRelease("G" + keyboardPosition, "8n");
-      
+      synth.triggerAttackRelease("G" + 4, "8n");
+      wKPress(".G");
       break;
     case 104:
-      synth.triggerAttackRelease("A" + keyboardPosition, "8n");
-      
+      synth.triggerAttackRelease("A" + 4, "8n");
+      wKPress(".A");
       break;
     case 106:
-      synth.triggerAttackRelease("B" + keyboardPosition, "8n");
-      
+      synth.triggerAttackRelease("B" + 4, "8n");
+      wKPress(".B");
       break;
     case 107:
-      synth.triggerAttackRelease("C" + (keyboardPosition + 1), "8n");
-      
+      synth.triggerAttackRelease("C" + 5, "8n");
+      wKPress(".octave-up-C");
       break;
     case 108:
-      synth.triggerAttackRelease("D" + (keyboardPosition + 1), "8n");
-      
+      synth.triggerAttackRelease("D" + 5, "8n");
+      wKPress(".octave-up-D");
       break;
     case 59:
-      synth.triggerAttackRelease("E" + (keyboardPosition + 1), "8n");
-     
+      synth.triggerAttackRelease("E" + 5, "8n");
+      wKPress(".octave-up-E");
       break;
     case 39:
-      synth.triggerAttackRelease("F" + (keyboardPosition + 1), "8n");
-      
+      synth.triggerAttackRelease("F" + 5, "8n");
+      wKPress(".octave-up-F");
       break;
 
 
     case 99:
-      synth.triggerAttackRelease("A" + (keyboardPosition + 2), "8n");
-     
+      synth.triggerAttackRelease("G" + 5, "8n");
+      wKPress(".octave-up-A2");
       break;
     case 118:
-      synth.triggerAttackRelease("B" + (keyboardPosition + 2), "8n");
-      
+      synth.triggerAttackRelease("A" + 5, "8n");
+      wKPress(".octave-up-B2");
       break;
     case 98:
-      synth.triggerAttackRelease("C" + (keyboardPosition + 2), "8n");
-      
+      synth.triggerAttackRelease("B" + 5, "8n");
+      wKPress(".octave-up-C2");
       break;
 
     case 119:
-      synth.triggerAttackRelease("C#" + keyboardPosition, "8n");
-     
+      synth.triggerAttackRelease("C#" + 4, "8n");
+      bKPress(".C-sharp");
       break;
     case 101:
-      synth.triggerAttackRelease("D#" + keyboardPosition, "8n");
-     
+      synth.triggerAttackRelease("D#" + 4, "8n");
+      bKPress(".D-sharp");
       break;
     case 116:
-      synth.triggerAttackRelease("F#" + keyboardPosition, "8n");
-      
+      synth.triggerAttackRelease("F#" + 4, "8n");
+      bKPress(".F-sharp");
       break;
     case 121:
-      synth.triggerAttackRelease("G#" + keyboardPosition, "8n");
-     
+      synth.triggerAttackRelease("G#" + 4, "8n");
+      bKPress(".G-sharp");
       break;
     case 117:
-      synth.triggerAttackRelease("A#" + keyboardPosition, "8n");
-      
+      synth.triggerAttackRelease("A#" + 4, "8n");
+      bKPress(".A-sharp");
       break;
     case 111:
-      synth.triggerAttackRelease("C#" + (keyboardPosition + 1), "8n");
-      
+      synth.triggerAttackRelease("C#" + 5, "8n");
+      bKPress(".octave-up-C-sharp");
       break;
     case 112:
-      synth.triggerAttackRelease("D#" + (keyboardPosition + 1), "8n");
-      
+      synth.triggerAttackRelease("D#" + 5, "8n");
+      bKPress(".octave-up-D-sharp");
       break;
     case 110:
-      synth.triggerAttackRelease("F#" + (keyboardPosition + 1), "8n");
-     
+      synth.triggerAttackRelease("F#" + 5, "8n");
+      bKPress(".octave-up-F-sharp");
       break;
     case 109:
-      synth.triggerAttackRelease("G#" + (keyboardPosition + 1), "8n");
-     
+      synth.triggerAttackRelease("G#" + 5, "8n");
+      bKPress(".octave-up-G-sharp");
       break;
     case 44:
-      synth.triggerAttackRelease("A#" + (keyboardPosition + 2), "8n");
-     
+      synth.triggerAttackRelease("A#" + 5, "8n");
+      bKPress(".octave-up-A-sharp");
       break;
-    default:
-     
+    
+
+
   };
 
   frequency = synth.frequency.value;
   
 }
 
-const whiteKeyPress = (selector) => {
+const wKPress = (selector) => {
   document.querySelector(selector).style.fill = "#5E503F";
   setTimeout(function(){
-    document.querySelector(selector).style.fill = "#C6AC8F";
+  document.querySelector(selector).style.fill = "#C6AC8F";
   }, 200)
 }
 
-const blackKeyPress = (selector) => {
+const bKPress = (selector) => {
   document.querySelector(selector).style.fill = "#22333B";
   setTimeout(function(){
-    document.querySelector(selector).style.fill = "#0A0908";
+  document.querySelector(selector).style.fill = "#0A0908";
   }, 200)
 }
 
